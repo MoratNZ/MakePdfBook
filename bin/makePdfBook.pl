4984d770789e60ee09fc6154ca97e1200e740f52
morat <david@verso.org>
2021-04-17 21:08:03 +1200

    Adds passthrough of revision highlighting to the pdf

--- bin/makePdfBook.pl
+++ bin/makePdfBook.pl
@@ -146,7 +146,10 @@ foreach my $chapter (@chapters){
         $line =~s/&#8804;/SYMBOLlessThanOrEqualToSYMBOL/g;
         $line =~ s/<div class="thumbinner".*?(<img src=.*?\/>).*?<div class="thumbcaption"><div.*?<a.*?<\/a><\/div>(.*?)<\/div>/<figure>\1<figcaption>\2<\/figcaption><\/figure>/;
 #<div class="thumbinner" style="width:302px;"><a href="/mediawiki/index.php/File:Fencing_Handbook_2020_Figure_1.png" class="image"><img src="/var/lib/mediawiki-1.34.0/images/thumb/3/3f/Fencing_Handbook_2020_Figure_1.png/300px-Fencing_Handbook_2020_Figure_1.png" decoding="async" width="300" height="291" class="thumbimage" srcset="/mediawiki/images/thumb/3/3f/Fencing_Handbook_2020_Figure_1.png/450px-Fencing_Handbook_2020_Figure_1.png 1.5x, /mediawiki/images/thumb/3/3f/Fencing_Handbook_2020_Figure_1.png/600px-Fencing_Handbook_2020_Figure_1.png 2x" /></a>  <div class="thumbcaption"><div class="magnify"><a href="/mediawiki/index.php/File:Fencing_Handbook_2020_Figure_1.png" class="internal" title="Enlarge"></a></div>Figure 1. With the handle vertical, the tip must touch the ground. In this example, the sword on the left is allowed, the sword on the right is not.</div></div><
-
+        
+        # Translation to preserve revision marking
+        $line =~ s/<span class="revision">(.*?)<\/span>/HLSTART\1HLSTOP/ig;
+        # <span class="revision">Discuss proposed changes with the Kingdom Armoured Combat Marshal and the Earl Marshal.</span>
 
 
         push @output, $line;
@@ -191,8 +194,6 @@ if($result){
         if($file_content =~ /\<h1\>(.*?)\<\/h1\>/){
             $title_guess = $1;
         }
-
-
 
         my $start_pos;
         my $end_pos = length $file_content;
@@ -314,6 +315,7 @@ my $content = join "", @lines;
 $content =~ s/\"(.*?)\"/``$1''/gs;
 
 $content =~ s/SYMBOLlessThanOrEqualToSYMBOL/\$\\leq\$/gs;
+$content =~ s/HLSTART(.*?)HLSTOP/\\textcolor{red}{\1}/gs;
 
 
 open( my $out, '>:encoding(utf8)', 'book.tex') or die("Unable to open file book.tex to wrwite revised version - $!");
