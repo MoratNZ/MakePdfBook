414ad5d091e888bbc8b085968b00e8b61a9c475b
David Maclagan <david@verso.org>
2024-06-22 07:03:26 +1200

    Renames MakePdfBook.php to SpecialMakePdfBook. Starts splitting out functions to MakePdfHelpers.php

--- src/SpecialMakePdfBook.php
+++ src/SpecialMakePdfBook.php
@@ -1,5 +1,6 @@
 <?php
 use MediaWiki\MediaWikiServices;
+use MakePdfBookHelpers;
 
 class SpecialMakePdfBook extends SpecialPage
 {
@@ -10,10 +11,10 @@ class SpecialMakePdfBook extends SpecialPage
 	{
 		parent::__construct('MakePdfBook');
 		$this->config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('MakePdfBook');
-		$this->parser = \MediaWiki\MediaWikiServices::getInstance()->getParser();
+		$this->parser = MediaWikiServices::getInstance()->getParser();
 
 	}
-	public function execute($par)
+	public function execute()
 	{
 		$request = $this->getRequest();
 		$output = $this->getOutput();
@@ -80,7 +81,7 @@ class SpecialMakePdfBook extends SpecialPage
 	}
 	private function returnCategoryJson($category)
 	{
-		$categories = $this->getBooks($category);
+		$categories = MakePdfBookHelpers::getBooks($category);
 		arsort($categories);
 
 		$output = $this->getOutput();
@@ -92,7 +93,7 @@ class SpecialMakePdfBook extends SpecialPage
 	private function generateCategoryListPage()
 	{
 		global $wgServer, $wgScriptPath;
-		$handbooks = $this->getBooks();
+		$handbooks = MakePdfBookHelpers::getBooks();
 
 		arsort($handbooks);
 
@@ -134,101 +135,6 @@ class SpecialMakePdfBook extends SpecialPage
 
 		foreach ($result as $row) {
 			$articles[] = Title::newFromID($row->cl_from);
-		}
-
-		return $articles;
-	}
-	private function getBooks($category = false)
-	{
-		$articles = array();
-
-		$instance = MediaWikiServices::getInstance();
-		$lb = $instance->getDBLoadBalancer();
-		$dbr = $lb->getConnection(DB_REPLICA);
-
-		$whereClause = [
-			"cat_title like '%book%'",
-			"cl_sortkey_prefix not like '%titlepage%'",
-		];
-		if ($category) {
-			$whereClause[] = "cat_title =\"$category\""; # TODO: replace with an expr() call once we get MW 1.42
-		}
-
-		# Get the non-titlepage pages
-		$query = $dbr->newSelectQueryBuilder()
-			->select([
-				'cat_title',
-				'page_id',
-				'cl_sortkey_prefix',
-			])
-			->from('page')
-			->join('categorylinks', null, 'page_id=cl_from')
-			->join('category', null, 'cl_to=cat_title')
-			->where($whereClause)
-			->caller('MakePdfBook');
-		$result = $query->fetchResultSet();
-
-
-		foreach ($result as $row) {
-			$category = $row->cat_title;
-			$page_id = $row->page_id;
-			$sortKey = $row->cl_sortkey_prefix;
-
-			$page = Title::newFromID($page_id);
-
-			if (!array_key_exists($category, $articles)) {
-				$articles[$category] = [
-					'title' => $category,
-					'chapters' => []
-				];
-			}
-
-			$articles[$category]['chapters'][$sortKey] = [
-				'title' => $page->getText(),
-				'sortKey' => $sortKey,
-				'url' => $page->getFullUrl(),
-			];
-		}
-		# Get the titlepage pages
-		#
-		# This is being done like this so that we can have fuzzy titlepage
-		# labelling in the wiki, but precise identification of titlepages here
-
-		$whereClause[1] = "cl_sortkey_prefix like '%titlepage%'";
-		$query = $dbr->newSelectQueryBuilder()
-			->select([
-				'cat_title',
-				'page_id',
-				'cl_sortkey_prefix',
-			])
-			->from('page')
-			->join('categorylinks', null, 'page_id=cl_from')
-			->join('category', null, 'cl_to=cat_title')
-			->where($whereClause)
-			->caller('MakePdfBook');
-
-		$result = $query->fetchResultSet();
-
-		foreach ($result as $row) {
-			$category = $row->cat_title;
-			$page_id = $row->page_id;
-			$sortKey = $row->cl_sortkey_prefix;
-
-			$page = Title::newFromID($page_id);
-
-			$titlePage = [
-				'title' => $page->getText(),
-				'url' => $page->getFullUrl(),
-			];
-			if (!array_key_exists($category, $articles)) {
-				$articles[$category] = [
-					'title' => $category,
-					'chapters' => [],
-					'titlepage' => $titlePage
-				];
-			} else {
-				$articles[$category]['titlepage'] = $titlePage;
-			}
 		}
 
 		return $articles;
