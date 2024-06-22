b63c48dfe92f9664c271f39a7efa61896f854388
David Maclagan <david@verso.org>
2024-06-22 17:43:58 +1200

    Adds constructor and setup functions to Book

--- src/Book.php
+++ src/Book.php
@@ -1,105 +1,28 @@
 <?php
 namespace MediaWiki\Extension\MakePdfBook;
 
-
 use MediaWiki\MediaWikiServices;
 use Chapter;
 
 class Book
 {
-    public static function getBooks($category = false)
+    public function __construct(string $category)
     {
-        $articles = array();
-
-        $instance = MediaWikiServices::getInstance();
-        $lb = $instance->getDBLoadBalancer();
-        $dbr = $lb->getConnection(DB_REPLICA);
-
-        $whereClause = [
-            "cat_title like '%book%'",
-            "cl_sortkey_prefix not like '%titlepage%'",
-        ];
-        if ($category) {
-            $whereClause[] = "cat_title =\"$category\""; # TODO: replace with an expr() call once we get MW 1.42
-        }
-
-        # Get the non-titlepage pages
-        $query = $dbr->newSelectQueryBuilder()
-            ->select([
-                'cat_title',
-                'page_id',
-                'cl_sortkey_prefix',
-            ])
-            ->from('page')
-            ->join('categorylinks', null, 'page_id=cl_from')
-            ->join('category', null, 'cl_to=cat_title')
-            ->where($whereClause)
-            ->caller('MakePdfBook');
-        $result = $query->fetchResultSet();
-
-
-        foreach ($result as $row) {
-            $category = $row->cat_title;
-            $page_id = $row->page_id;
-            $sortKey = $row->cl_sortkey_prefix;
-
-            $page = \Title::newFromID($page_id);
-
-            if (!array_key_exists($category, $articles)) {
-                $articles[$category] = [
-                    'title' => $category,
-                    'chapters' => []
-                ];
-            }
-
-            $articles[$category]['chapters'][$sortKey] = [
-                'title' => $page->getText(),
-                'sortKey' => $sortKey,
-                'url' => $page->getFullUrl(),
-            ];
-        }
-        # Get the titlepage pages
-#
-# This is being done like this so that we can have fuzzy titlepage
-# labelling in the wiki, but precise identification of titlepages here
-
-        $whereClause[1] = "cl_sortkey_prefix like '%titlepage%'";
-        $query = $dbr->newSelectQueryBuilder()
-            ->select([
-                'cat_title',
-                'page_id',
-                'cl_sortkey_prefix',
-            ])
-            ->from('page')
-            ->join('categorylinks', null, 'page_id=cl_from')
-            ->join('category', null, 'cl_to=cat_title')
-            ->where($whereClause)
-            ->caller('MakePdfBook');
-
-        $result = $query->fetchResultSet();
-
-        foreach ($result as $row) {
-            $category = $row->cat_title;
-            $page_id = $row->page_id;
-            $sortKey = $row->cl_sortkey_prefix;
-
-            $page = \Title::newFromID($page_id);
-
-            $titlePage = [
-                'title' => $page->getText(),
-                'url' => $page->getFullUrl(),
-            ];
-            if (!array_key_exists($category, $articles)) {
-                $articles[$category] = [
-                    'title' => $category,
-                    'chapters' => [],
-                    'titlepage' => $titlePage
-                ];
-            } else {
-                $articles[$category]['titlepage'] = $titlePage;
-            }
-        }
-
-        return $articles;
+        $this->category = $category;
+        $this->chapters = [];
+        $this->titlepage = null;
+    }
+    public function setTitlepage(Chapter $chapter)
+    {
+        $this->titlepage = $chapter;
+        return $this;
+    }
+    public function addChapter($title, $sortKey)
+    {
+        $this->chapters[$title] = new Chapter($this, $title, $sortKey);
+        return $this;
+    }
+    public function render()
+    {
     }
 }
