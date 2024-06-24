2127bae857dd238fe0624ecdea85d93c306ffd8a
David Maclagan <david@verso.org>
2024-06-24 12:01:56 +1200

    renames renderpdf for consistency

--- src/SpecialMakePdfBook.php
+++ src/SpecialMakePdfBook.php
@@ -32,7 +32,7 @@ class SpecialMakePdfBook extends SpecialPage
 				case false:
 					return $this->buildSpecialPage();
 				case "render":
-					return $this->renderPdf($parsedUrl['target'], $parsedUrl['parameters']);
+					return $this->servePdf($parsedUrl['target'], $parsedUrl['parameters']);
 				case "json":
 					return $this->buildJsonResponse($parsedUrl['target'], $parsedUrl['parameters']);
 				case "testoutput":
@@ -51,48 +51,6 @@ class SpecialMakePdfBook extends SpecialPage
 				)
 			);
 		}
-		// $request = $this->getRequest();
-		// $output = $this->getOutput();
-		// $this->setHeaders();
-
-		// $cacheFileDir = $this->config->get('MakePdfBookCacheFileDir');
-
-		// $errorText = "";
-
-		// # Get request data 
-
-		// try {
-		// 	# Get articles from category
-		// 	$articles = $this->getCategoryArticles($category);
-
-		// 	# Get the cache file name
-		// 	$cacheFile = $this->getCacheHash($articles) . ".pdf";
-
-		// 	// If the file doesn't exist, render the content now
-		// 	if ($forceRebuild || !file_exists("$cacheFileDir/$cacheFile")) {
-		// 		$this->renderPdf($cacheFileDir, $cacheFile, $category, $articles);
-		// 		if (!file_exists("$cacheFileDir/$cacheFile")) {
-		// 			throw new Exception("PDF generation has somehow silently failed. I am confused and ashamed.");
-		// 		}
-		// 	}
-		// 	# Return the PDF, or an error page if generation has failed.
-		// } catch (\Throwable $e) {
-		// 	$errorText = $e->getMessage();
-		// }
-		// $output->disable();
-
-		// if ($testPdfOutput) {
-		// 	header("Content-type: application/pdf");
-		// 	readfile("/home/morat/MakePdfBook/test/marshal_handbook.pdf");
-		// } else if ($errorText) {
-		// 	header("Content-type: text/html; charset=utf-8");
-		// 	print "<html><head></head><body><h1>Error creating PDF book</h1>";
-		// 	print "<pre>$errorText</pre>";
-		// 	print "</body></html>";
-		// } else {
-		// 	header("Content-type: application/pdf");
-		// 	readfile("$cacheFileDir/$cacheFile");
-		// }
 	}
 	public function getGroupName()
 	{
@@ -150,7 +108,6 @@ class SpecialMakePdfBook extends SpecialPage
 
 		$this->getOutput()->addWikiTextAsInterface($textString);
 	}
-
 	private function buildJsonResponse($category, $params)
 	{
 		$jsonText = json_encode(['hello', "world"]);
@@ -218,114 +175,7 @@ class SpecialMakePdfBook extends SpecialPage
 
 		$this->getOutput()->addWikiTextAsInterface($textString);
 	}
-	private function getCategoryArticles($category)
-	{
-		$articles = array();
-
-		$lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
-		$dbr = $lb->getConnectionRef(DB_REPLICA);
-
-		$result = $dbr->newSelectQueryBuilder()
-			->select('cl_from')
-			->from('categorylinks')
-			->where([
-				$dbr->expr("cl_to", "=", $category),
-				$dbr->expr("cl_sortkey_prefix", "not like", "%titlepage%"),
-			])
-			->orderBy('cl_sortkey')
-			->caller('MakePdfBook')
-			->fetchResultSet();
-
-		foreach ($result as $row) {
-			$articles[] = Title::newFromID($row->cl_from);
-		}
-
-		return $articles;
-	}
-	private function getCategoryTitlePage($category)
-	{
-		$lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
-		$dbr = $lb->getConnectionRef(DB_REPLICA);
-
-		$result = $dbr->newSelectQueryBuilder()
-			->select('cl_from')
-			->from('categorylinks')
-			->where(["cl_to = " . $dbr->addQuotes($category), 'cl_sortkey_prefix like "%titlepage%"'])
-			->caller('MakePdfBook')
-			->fetchResultSet();
-
-		$numRows = $result->numRows();
-		if ($numRows == 0) {
-			return NULL;
-		} else if ($numRows == 1) {
-			$row = $result->fetchRow();
-			return \Title::newFromID($row[0]);
-		} else {
-			throw new Exception("There is more than one article in category $category labelled with sort key 'titlepage'. Please trim that down to one.");
-		}
-	}
-	private function getCacheHash($articles)
-	{
-		$cacheString = '\nFile sig: ' . md5(file_get_contents(__FILE__)); // the contents of the rendering code (this script),
-		$cacheString = '\nFile sig: ' . md5(file_get_contents(dirname(__FILE__) . "/../bin/template.tex")); // the contents of the tex template
-		foreach ($articles as $art) {
-			$cacheString .= "\n" . $art->getPrefixedText() . ': ' . $art->getLatestRevID(); // and the latest revision(s) of the article(s)
-		}
-		return md5($cacheString);
-	}
-
-	private function writeArticleHtmlFile($title, $fileName, $treatAsTitlepage = false)
-	{
-		global $wgUploadDirectory, $wgScriptPath, $wgUser, $wgServer;
-
-		$scriptPath = $wgServer . $wgScriptPath;
-		$opt = ParserOptions::newFromAnon();
-
-		$titleText = $this->stripNameSpace($title->getPrefixedText());
-
-		$fullArticleUrl = $title->getFullUrl();
-		$article = new Article($title);
-		$text = $article->getPage()->getContent()->getNativeData();
-
-		$htmlText = $this->parser->parse($text, $title, $opt);
-
-		# This section is a bit of a black box, taken from the original PdfBook extension
-		####
-		$text .= "__NOTOC__";
-		#$out = $wgParser->parse($text, $title, $opt, true, true);
-		$text = $htmlText->getText($options = [
-			'allowTOC' => false,
-			'enableSectionEditLinks' => false,
-			'unwrap' => false,
-			'deduplicateStyles' => true,
-		]);
-		$urlPath = parse_url($scriptPath, PHP_URL_PATH);
-		$imgpath = str_replace('/', '\/', $urlPath . '/' . basename($wgUploadDirectory)); // the image's path
-		$text = preg_replace("|(<img[^>]+?src=\"$imgpath)(/.+?>)|", "<img src=\"$wgUploadDirectory$2", $text);
-
-		$titleText = basename($titleText);
-		$h1 = "<center><h1>$titleText</h1></center>";
-		if ($treatAsTitlepage) {
-			$html = utf8_decode("$text\n");
-		} else {
-			$html = utf8_decode("$h1  $text\n");
-		}
-
-		file_put_contents($fileName, $html);
-	}
-	private function stripNameSpace($titleText)
-	{
-		// This is a filthy filthy hack to remove namespace labelling from page titles
-		// the correct solution is to access the DISPLAYTITLE
-		$colonPosition = strpos($titleText, ':');
-		if ($colonPosition) {
-			return substr($titleText, $colonPosition + 1);
-		} else {
-			return $titleText;
-		}
-	}
-
-	private function renderPdf($category, $params)
+	private function servePdf($category, $params)
 	{
 		# get the book and load its content
 		$book = $this->bookSet->getBook($category)
@@ -346,43 +196,5 @@ class SpecialMakePdfBook extends SpecialPage
 		$this->getOutput()->disable();
 		header("Content-type: application/pdf");
 		readfile($pdfFile);
-
-		// global $wgServer, $wgScriptPath, $makepdfIsDraft;
-		// $titlePage = $this->getCategoryTitlePage($category);
-
-		// $baseTempFileDir = $this->config->get('MakePdfBooktempFileDir');
-
-		// // Create the holding directory for our temp files
-		// $tempFileDir = $this->createEmptyDirectory($baseTempFileDir, $category);
-
-		// // Create the content temp files
-		// if ($titlePage) {
-		// 	$titlepageFileName = "$tempFileDir/titlepage.html";
-		// 	$this->writeArticleHtmlFile($titlePage, $titlepageFileName, true);
-		// }
-
-		// $articleCount = 0;
-		// foreach ($articles as $title) {
-		// 	$articleCount++;
-		// 	$chapterFileName = "$tempFileDir/chapter-$articleCount.html";
-
-		// 	$this->writeArticleHtmlFile($title, $chapterFileName);
-		// }
-
-		// // Copy the template file to the holding dir
-		// if ($makepdfIsDraft) {
-		// 	copy(dirname(__FILE__) . "/../bin/draft_template.tex", "$tempFileDir/template.tex");
-		// } else {
-		// 	copy(dirname(__FILE__) . "/../bin/template.tex", "$tempFileDir/template.tex");
-		// }
-
-
-		// // Call out to the book assembler
-		// $cmd = dirname(__FILE__) . "/../bin/makePdfBook.pl $tempFileDir $outputDir/$outputFileName 2>&1";
-		// $shellResult = shell_exec($cmd);
-
-		// if ($shellResult) {
-		// 	throw new Exception($shellResult);
-		// }
 	}
 }
